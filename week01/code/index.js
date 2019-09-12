b66bbe3fba971c3655019d54364cf9a18c49b48f
andresns <andresm.navarro@gmail.com>
2019-09-12 09:58:32 -0700

    create http server and configure debugger

--- week01/code/index.js
+++ week01/code/index.js
@@ -1,8 +1,17 @@
-"use strict";   //ES5. Makes the compiler more strict and helps us to debug more easily. With "use strict" enabled we can't declare accidental global variables.
-                //IMPORTANT: "use strict" must be the first thing in your js file, nothing else can precede it, or it won't work.
-                //Caveat (Advertencia): "use strict" can be used inside single functions or class bodies, but again, it must be the first thing in the body.
+"use strict";
+
+//"use strict" makes the compiler more strict and helps us to debug more easily. With "use strict" enabled we can't declare accidental global variables.
+//IMPORTANT: "use strict" must be the first thing in your js file, nothing else can precede it, or it won't work.
+//Caveat (Advertencia): "use strict" can be used inside single functions or class bodies, but again, it must be the first thing in the body.
 
 // npm init -f  initialize npm with default values. (Create package.json)
 // npm install eslint --save-dev   (create node_modules and package-lock.json)
 // .\node_modules\.bin\eslint --init   (create configuration file)
 
+//python -m http.server
+
+
+var a = 10;
+var b = 100;
+
+console.log(a + b);
