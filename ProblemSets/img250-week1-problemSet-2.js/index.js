aa40e3a8ff147d057fd40b644dbcbaa5af3bd74f
andresns <andresm.navarro@gmail.com>
2019-09-11 21:06:59 -0700

    fix errors in index.js

--- ProblemSets/img250-week1-problemSet-2.js/index.js
+++ ProblemSets/img250-week1-problemSet-2.js/index.js
@@ -1,32 +1,32 @@
 let four = 4;
 let fifty = 50;
 
-array = [four, fifty]
+var array = [four, fifty];
 
 var sumIsPositive = true;
 
 function range(start, end, step) {
-	newArray = [];
+	var newArray = [];
 	step = step != null ? step : 1;
-	let i = start;	
+	let i = start;
 	newArray.push(i);
 	while (i !== end) {
 		i += step;
-		newArray.push(i);    
+		newArray.push(i);
 	}
 	return newArray;
 }
 
 function sum(arr) {
-	let sum = 0   
-	for (let i=0; i<arr.length; i--) {
+	let sum = 0;
+	for (let i = 0; i < arr.length; i--) {
 		sum += arr[i];
-        
+
 	}
 	return sum;
 }
 
-finalStatement = 'The sum is: '
+var finalStatement = 'The sum is: ';
 
 console.log(finalStatement + sum);
-console.log(`And it is ${sumIsPositive} that sum is positive.`)
+console.log(`And it is ${sumIsPositive} that sum is positive.`);
